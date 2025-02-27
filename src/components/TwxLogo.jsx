25618e85b3bad2622ccf18b689f8491298fe23e1
Thomassaiag <thomas.saiag@gmail.com>
2025-02-27 16:53:13 +0100

    added global class for smooth scroll

---
+++ src/components/TwxLogo.jsx
@@ -0,0 +1,13 @@
+import twxLogo from "../assets/twxLogo.svg";
+
+const TwxLogo = () => {
+	return (
+		<img
+			className="h-16 w-16 text-emerald-500"
+			src={twxLogo}
+			alt="thingworx logo"
+		/>
+	);
+};
+
+export default TwxLogo;
