5fea30c11416e3bdd68c76c5728dbff159b550cd
Thomassaiag <thomas.saiag@gmail.com>
2025-02-27 12:55:42 +0100

    added style to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,21 @@ import { links } from "../../data";
 
 const Navbar = () => {
 	return (
-		<nav className="bg-emerald-200">
-			<div className="mx-auto columns-4 gap-20">
-				{links.map((link) => {
-					const { id, href, text } = link;
-					return (
-						<a href={href} key={id}>
-							{text}
-						</a>
-					);
-				})}
+		<nav className="bg-emerald-100">
+			<div className="mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8">
+				<h2 className="text-3xl font-bold">
+					WEB<span className="text-emerald-600">DEV</span>
+				</h2>
+				<div className="flex gap-x-3">
+					{links.map((link) => {
+						const { id, href, text } = link;
+						return (
+							<a className="capitalize text-lg tracking-wide hover:text-emerald-600 duration-300" href={href} key={id}>
+								{text}
+							</a>
+						);
+					})}
+				</div>
 			</div>
 		</nav>
 	);
