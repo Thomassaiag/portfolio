c7d3a00e6577806146eef749664745936eafea13
Thomassaiag <thomas.saiag@gmail.com>
2025-02-27 12:32:30 +0100

    started nevbar with data information

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { links } from "../../data";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+	return (
+		<nav className="bg-emerald-200">
+			<div className="mx-auto columns-4 gap-20">
+				{links.map((link) => {
+					const { id, href, text } = link;
+					return (
+						<a href={href} key={id}>
+							{text}
+						</a>
+					);
+				})}
+			</div>
+		</nav>
+	);
+};
 
-export default Navbar
+export default Navbar;
