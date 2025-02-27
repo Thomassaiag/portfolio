380126b8b27f8a3dd6cfc567c5f4a2a15882d1d1
Thomassaiag <thomas.saiag@gmail.com>
2025-02-27 17:07:34 +0100

    finalised about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
+import About from "./components/About";
 
 const App = () => {
 	return (
 		<>
 			<Navbar />
-            <Hero/>
-            <Skills/>
-            
+			<Hero />
+			<Skills />
+			<About />
 		</>
 	);
 };
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,24 @@
-
+import SectionTitle from "./SectionTitle";
+import aboutImg from "../assets/about.svg";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+	return (
+		<section className="bg-white py-20" id="about">
+			<div className="align-element grid md:grid-cols-2 items-center gap-16">
+				<img src={aboutImg} alt="about" className="w-full h-64" />
+				<article>
+					<SectionTitle text="About me" />
+					<p className="text-slate-600 mt-8 leading-loose">
+						Lorem, ipsum dolor sit amet consectetur adipisicing
+						elit. Consequatur provident et ratione, corporis
+						temporibus non, eos vitae facilis ipsa debitis rem dolor
+						autem molestiae. Dolor alias impedit perferendis ex
+						cupiditate.
+					</p>
+				</article>
+			</div>
+		</section> 
+	);
+};
 
-export default About
+export default About;
