56834b3bd3acc4caf3e5202e1809eb3f68bd9ef0
Thomassaiag <thomas.saiag@gmail.com>
2025-02-28 10:59:31 +0100

    finalise resume section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
 	return (
 		<section className="bg-white py-20" id="about">
 			<div className="align-element grid md:grid-cols-2 items-center gap-16">
-				<img src={aboutImg} alt="about" className="w-full h-64" />
+				<img src={aboutImg} alt="about" className="image" />
 				<article>
 					<SectionTitle text="About me" />
 					<p className="text-slate-600 mt-8 leading-loose">
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,23 +1,44 @@
 import React from "react";
 import SectionTitle from "./SectionTitle";
 import { TbFileCv } from "react-icons/tb";
-
+import { IoMdEye } from "react-icons/io";
 
 const resumeUrl = "/CV_Thomas_Saiag.pdf";
+import resumeImg from "../assets/resume.svg";
 
 const Resume = () => {
 	return (
-		<div className="align-element" id="resume">
-			<SectionTitle text="Resume" />
-			<div>
-				<h3>You can dowload my Resume</h3>
-				<a href={resumeUrl} download="CV_Thomas_Saiag.pdf">
-					Click here
-				</a>
-                <h3>Or you can view it directly:</h3>
-                <a href={resumeUrl} target="_blank" rel='noopener noreferrer'><TbFileCv className="icons"/></a>
+		<section className="py-20 align-element " id="resume">
+			<div className="grid md:grid-cols-2 gap-8">
+				<img className="image" src={resumeImg} alt="resume image" />
+
+				<article>
+					<SectionTitle text="Resume" />
+					<div className="grid md:grid-cols-2 gap-8">
+						<div>
+							<p className="text-slate-600 mt-8 leading-loose mb-6">
+								You can dowload my Resume
+							</p>
+							<a href={resumeUrl} download="CV_Thomas_Saiag.pdf">
+								<TbFileCv className="icons" />
+							</a>
+						</div>
+						<div>
+							<p className="text-slate-600 mt-8 leading-loose mb-6">
+								Or, you can view it directly
+							</p>
+							<a
+								href={resumeUrl}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<IoMdEye className="icons" />
+							</a>
+						</div>
+					</div>
+				</article>
 			</div>
-		</div>
+		</section>
 	);
 };
 
